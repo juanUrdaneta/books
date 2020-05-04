d23df65020a1baf6f343f90bcdb5114e0b0a05e3
Juan Urdaneta <juankorus@gmail.com>
2020-05-04 14:46:17 -0300

    handled empty results

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -4,6 +4,9 @@ import Card from './Card'
 const BookList = ({list}) => {
   return ( 
     <div className="book-list">
+      {list.length===0 && (
+        <h2>Nothing to display</h2>
+      )}
       {
         list.map(e=>(
           <Card info={e.volumeInfo} />
--- src/functions/fetch.js
+++ src/functions/fetch.js
@@ -1,5 +1,5 @@
 const getBooks = async searchQuery => {
-  const APIKEY = 'USE YOUR GOOGLE BOOKS API KEY';
+  const APIKEY = 'USE YOUR GOOGLE BOOKS API KEY HERE';
   
   const fe = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&key=${APIKEY}`);
   const data = (fe.ok) ? await fe.json() : 'none';
